0e6ee96ebf4c34f808f1370e5ea0f5ae55bb7fe6
FATIMA-EZ-ZAHRA <101366220+FATIMA-EZ-ZAHRA@users.noreply.github.com>
2022-06-27 04:08:45 +0000

    Update settings.py

--- conference/settings.py
+++ conference/settings.py
@@ -86,17 +86,26 @@ WSGI_APPLICATION = 'conference.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
+""" DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'u500547363_django',
-        'USER': 'u500547363_django',
-        'PASSWORD': '9YvMnb~z',
-        'HOST': 'sql480.main-hosting.eu',
+        'NAME': 'conferences_api',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
         'PORT': '3306',
     }
+} """
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'conferences_api', 
+        'USER': 'postgres', 
+        'PASSWORD': 'samadi-13051989',
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
+    }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
@@ -132,7 +141,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-# STATIC_URL = 'static/'
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
@@ -140,16 +149,3 @@ USE_TZ = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-
-
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
